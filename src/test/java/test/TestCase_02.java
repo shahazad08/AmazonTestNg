4f067f23f024deed22f828c5448c34687e84b079
shahazad08 <sk.shahazad@gmail.com>
2025-12-03 11:47:32 +0530

    Added: TC2 basic code

--- src/test/java/test/TestCase_02.java
+++ src/test/java/test/TestCase_02.java
@@ -6,6 +6,13 @@ import org.openqa.selenium.WebDriver;
 public class TestCase_02 {
 
 	public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.amazon.in/");
+		if(driver.getTitle().contains("Amazon.in"))
+			System.out.println("Title Matches");
+		else 
+			System.out.println("Title doesn't Matches");
+		driver.quit();
 	
 		// TODO Auto-generated method stub
 	}
